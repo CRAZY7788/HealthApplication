6fe92f21ecebe386902e90358ce78d6904435a90
liwei hao <haoliweicool@gmail.com>
2017-02-04 20:50:09 -0600

    config finished.

--- Application/src/com/example/HealthApplication/HealthMainActivity.java
+++ Application/src/com/example/HealthApplication/HealthMainActivity.java
@@ -3,6 +3,8 @@ package com.example.HealthApplication;
 import android.app.Activity;
 import android.os.Bundle;
 
+import static com.example.HealthApplication.R.*;
+
 public class HealthMainActivity extends Activity {
     /**
      * Called when the activity is first created.
@@ -10,6 +12,9 @@ public class HealthMainActivity extends Activity {
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
+        setContentView(layout.main);
+    }
+    public void test(){
+        
     }
 }
